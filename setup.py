653428e073b11545233227d4ae419737e3a78044
johnsam7 <johnsam@mit.edu>
2022-09-12 19:29:20 -0400

    added installation from source of antspy 0.2.9 to setup file

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@
 import sys
 
 import os
+import pooch
 import os.path as op
 
 from setuptools import setup
@@ -25,8 +26,23 @@ with open(op.join('cmb', '_version.py'), 'r') as fid:
 if version is None:
     raise RuntimeError('Could not determine version')
 
+# Download and install AntsPy 0.2.9 from source
+from pooch import retrieve
+import zipfile
+print('Installing ANTsPy...')
+path = op.dirname(__file__)
+os.system('mkdir ' + path + '/tmp')
+retrieve(url='https://github.com/ANTsX/ANTsPy/archive/refs/tags/v0.2.9.zip',
+         known_hash=None, fname='antspy.zip', path=path + '/tmp')
+with zipfile.ZipFile(path + '/tmp/' + 'antspy.zip', 'r') as zip_ref:
+    zip_ref.extractall(path + '/tmp')
+workdir = os.popen('pwd').read()[:-1]
+os.system('cd ' + path + '/tmp/ANTsPy-0.2.9/')
+os.system('python setup.py install')
+os.system('cd ' + workdir)
 
-descr = """Cere-MEG-Bellum (CMB) - Cerbellum segmentation and forward solution calculation"""
+
+descr = """Cere-MEG-Bellum (CMB) - Cerebellum segmentation and forward solution calculation"""
 
 DISTNAME = 'cmb'
 DESCRIPTION = descr
