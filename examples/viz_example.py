f49251b8ee1ba66497c7aaaf2f90afd72d00315b
gabrielbmotta <gabrielbenmotta@gmail.com>
2023-10-06 10:19:58 -0400

    simplify example, fix update viz beofre saving screeshot

--- examples/viz_example.py
+++ examples/viz_example.py
@@ -1,67 +1,43 @@
 import numpy as np
 from cmb import MLabEmulator
 
+mlab = MLabEmulator()
 
-def get_figures():
-    mlab = MLabEmulator()
+mlab.figure(bgcolor=(1., 1., 1.),
+            fgcolor=(0., 0., 0.),
+            size=(1200, 1200))
 
-    mlab.figure(bgcolor=(1., 1., 1.),
-                fgcolor=(0., 0., 0.),
-                size=(1200, 1200))
+print("Making bogus data...")
+n = 8
+t = np.linspace(-np.pi, np.pi, n)
+z = np.exp(1j * t)
+x = z.real.copy()
+y = z.imag.copy()
+z = np.zeros_like(x)
 
-    print("Making bogus data...")
-    n = 8
-    t = np.linspace(-np.pi, np.pi, n)
-    z = np.exp(1j * t)
-    x = z.real.copy()
-    y = z.imag.copy()
-    z = np.zeros_like(x)
+triangles = [(0, i, i + 1) for i in range(1, n)]
+x = np.r_[0, x]
+y = np.r_[0, y]
+z = np.r_[1, z]
+t = np.r_[0, t]
 
-    triangles = [(0, i, i + 1) for i in range(1, n)]
-    x = np.r_[0, x]
-    y = np.r_[0, y]
-    z = np.r_[1, z]
-    t = np.r_[0, t]
+figure = mlab.triangular_mesh(x, y, z, triangles,
+                              scalars=t, colormap='bwr')
 
-    figures = list()
+# We can set camera position using angles
+figure.camera.azimuth = -90
+figure.camera.elevation = -90
 
-    figures.append(mlab.triangular_mesh(x, y, z, triangles,
-                                        scalars=t, colormap='bwr'))
+# Alternativley, we can set camera position using points.
+figure.camera.position = (3.0, 1.5, 2.5)
 
-    t2 = [3 for _ in t]
-    mlab.figure(bgcolor=(1., 1., 1.),
-                fgcolor=(0., 0., 0.),
-                size=(1200, 1200))
+# Depending on your 3D framework, you might need to
+# manually query an update.
+figure.app.processEvents()  # makes camera move requests take effect
+figure.render()  # forces a redraw
 
-    figures.append(mlab.triangular_mesh(x, y, z, triangles,
-                                        scalars=t2, colormap='bwr'))
+# Takes a screenshot of the current camera view
+figure.screenshot('my_screenshot')
 
-    mlab.colorbar()
-    return figures
-
-
-def take_screenshot(figure, file_name):
-    # We can set camera position using angles
-    figure.camera.azimuth = -90
-    figure.camera.elevation = -90
-
-    # Alternativley, we can set camera position using points.
-    figure.camera.position = (2.0, 1.0, 2.0)
-
-    # Depending on your 3D framework, you might need to
-    # manually query an update.
-    figure.update()
-
-    # Takes a screenshot of the current camera view
-    figure.screenshot(file_name)
-
-
-figures = get_figures()
-
-count = 0
-
-for figure in figures:
-    count += 1
-    take_screenshot(figure, f'screenshot_{count}')
 
 print("Done!")
