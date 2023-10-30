1d0de8ca048ff2671f8e84d513387aec4265dfbc
Gabriel B Motta <gbmotta@mgh.harvard.edu>
2023-10-30 11:44:47 -0400

    add readme as landing page, ignore build files

--- doc/conf.py
+++ doc/conf.py
@@ -6,6 +6,8 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import sphinx_bootstrap_theme
+
 project = 'Cere-MEG-Bellum'
 copyright = '2023, John Samuelsson'
 author = 'John Samuelsson'
@@ -19,9 +21,22 @@ templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+autosummary_generate = True
+autodoc_default_options = {'inherited-members': None}
+numpydoc_class_members_toctree = False
+numpydoc_attributes_as_param_list = True
+
+html_theme = 'bootstrap'
+
 html_static_path = ['_static']
+
+html_theme_options = {
+    'navbar_sidebarrel': False,
+    'navbar_links': [
+        ("GitHub", "https://github.com/jasmainak/cbm", True)
+    ],
+    'bootswatch_theme': "yeti"
+}
