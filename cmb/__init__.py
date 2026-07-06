bd3e1403e6daad358a6e99d1678005787cbf8988
Teemu Taivainen <ttaiv@outlook.com>
2026-07-06 14:52:44 +0300

    Update init

--- cmb/__init__.py
+++ cmb/__init__.py
@@ -1,13 +1,20 @@
-"""Cere-MEG-Bellum (CMB)"""
+"""Cere-MEG-Bellum (CMB)."""
 
 import os as _os
 
 from ._version import __version__
-
-# Default data directory: <package_dir>/ (atlas data goes into data/, nnUNet/, etc.)
-CMB_DATA_DIR = _os.path.join(_os.path.dirname(__file__), '')
-
 from .functions import get_cerebellum_data
 from .source_space import setup_full_source_space
-from .visualization import plot_cerebellum_data
 from .utils import is_float
+
+# Default data directory: <package_dir>/ (atlas data goes into data/, nnUNet/, etc.)
+CMB_DATA_DIR = _os.path.join(_os.path.dirname(__file__), "")
+
+
+__all__ = [
+    "CMB_DATA_DIR",
+    "get_cerebellum_data",
+    "setup_full_source_space",
+    "is_float",
+    "__version__",
+]
