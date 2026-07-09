a760fbca2bec0febc096b1651852cefc503ca4f9
Teemu Taivainen <ttaiv@outlook.com>
2026-07-09 09:57:35 +0300

    Add fetching cerebellum data

--- dev_tools/setup_source_space_with_mocking.py
+++ dev_tools/setup_source_space_with_mocking.py
@@ -7,7 +7,7 @@ from unittest.mock import patch
 
 from mne.datasets import sample
 
-from cmb import setup_full_source_space
+from cmb import get_cerebellum_data, setup_full_source_space
 
 cerebellum_data_dir = Path("dir/here")
 output_file = Path(__file__).parent / "reference_src.pkl"
@@ -35,6 +35,7 @@ def dummy_ants_registration(*args, **kwargs):
 @patch("source_space.ants.registration", new=dummy_ants_registration)
 def setup_and_save_source_space() -> None:
     """Set up the source space and save it to a pickle file."""
+    get_cerebellum_data(cerebellum_data_dir)
     # Make sure that these match to the parameters used when doing the reference
     # source space creation.
     spacing = 2
