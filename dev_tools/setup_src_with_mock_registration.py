1ac1f0fe2191bbc97776a2aebdc12580d17863dd
Teemu Taivainen <136599271+ttaiv@users.noreply.github.com>
2026-07-15 15:34:33 +0300

    Add tools for manual testing (#9)
    - Added script that runs `setup_full_source_space` with ANTs registration mocked.
      - Mocking gives speed and removes variability from results.
    - Added script for comparing two segmentation NIfTI files.
    - Added script for comparing two pickled source space objects.

---
+++ dev_tools/setup_src_with_mock_registration.py
@@ -0,0 +1,70 @@
+"""Set up full source space so that nondeterministic ANTs registration is mocked.
+
+Resulting source space is saved to a pickle file.
+
+This script can be utilized in testing. Created source spaces can be compared with
+`compare_source_spaces.py`. Furthermore, the final segmentation
+NIfTI files can be compared with `compare_segmentations.py`.
+
+NOTE: nnU-Net predictions are not mocked and might still cause nondeterministic
+behavior.
+"""
+
+import os.path as op
+import pickle
+from pathlib import Path
+from unittest.mock import patch
+
+from mne.datasets import sample
+
+from cmb import get_cerebellum_data, setup_full_source_space
+
+# Set path to cerebellum data directory and output file for the source space.
+cerebellum_data_dir = Path("dir/here")
+output_file = Path(__file__).parent / "reference_src.pkl"
+
+# Use MNE sample subject.
+data_path = sample.data_path()
+subjects_dir = op.join(data_path, "subjects")
+subject = "sample"
+
+
+def dummy_ants_registration(*args, **kwargs):
+    """Mock function to replace ANTs registration in tests.
+
+    Replaces the ANTs SyNCC registration.
+    Returns a fake dictionary that mimics what ANTs would output,
+    but with empty transform lists so the coordinates don't drift.
+    """
+    return {
+        "fwdtransforms": [],
+        "invtransforms": [],
+        "warpedmovout": kwargs.get("moving"),  # Just return the moving image unchanged
+    }
+
+
+@patch("ants.registration", new=dummy_ants_registration)
+def setup_and_save_source_space() -> None:
+    """Set up the source space and save it to a pickle file."""
+    get_cerebellum_data(cerebellum_data_dir)
+    # Make sure that these match to the parameters used when doing the reference
+    # source space creation.
+    spacing = 2
+    cerebellum_subsampling = "sparse"
+
+    src = setup_full_source_space(
+        subject,
+        subjects_dir,
+        cerebellum_data_dir,
+        cerb_subsampling=cerebellum_subsampling,
+        plot_cerebellum=False,
+        spacing=spacing,
+        debug_mode=False,
+    )
+
+    with open(output_file, "wb") as f:
+        pickle.dump(src, f)
+
+
+if __name__ == "__main__":
+    setup_and_save_source_space()
