a8a3c73dab9fe43011da0289a99f92bb9d98b711
Teemu Taivainen <ttaiv@outlook.com>
2026-07-16 19:20:09 +0300

    Revert "FIX morphing segmentation to subject space"
    Seems like it was not a fix

--- cmb/segmentation.py
+++ cmb/segmentation.py
@@ -350,7 +350,7 @@ def _segment_cerebellum(
 
     # Go back to subject space
     seg_reg_float: NDArray[np.float32] = ants.apply_transforms(
-        fixed=subject_brain_ants,
+        fixed=template_ants,
         moving=seg_complete_ants,
         transformlist=registration["invtransforms"],
         interpolator="genericLabel",
