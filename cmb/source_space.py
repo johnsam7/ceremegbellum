7aaa389c140ae04901da5d6b7ac831defa9179c1
Teemu Taivainen <ttaiv@outlook.com>
2026-09-02 10:50:56 +0300

    Fix typos

--- cmb/source_space.py
+++ cmb/source_space.py
@@ -81,7 +81,7 @@ def create_cerebellar_surface(
         If True, will overwrite any existing mesh file at the save location. If False
         (default), raises a `FileExistsError` if the mesh file already exists.
     registration_caching : bool, optional
-        If True, it will attemp to read cached registration transforms from disk, and if
+        If True, it will attempt to read cached registration transforms from disk, and if
         not found, will save the transforms to disk for future use. Defaults to False,
         which means that registration will be computed without caching.
 
@@ -195,7 +195,7 @@ def _create_cerebellar_surface(
     )
     from scipy import signal
 
-    # Cast to str for compatability.
+    # Cast to str for compatibility.
     registration_cache_dir_str = (
         os.fspath(registration_cache_dir)
         if registration_cache_dir is not None
